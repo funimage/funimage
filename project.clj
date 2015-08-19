c19978573e70ec09d788cfc49d43334b10efb559
Kyle Harrington <kyleh@cs.brandeis.edu>
2015-08-19 16:26:10 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funimage/funimage "0.1.61"
+(defproject funimage/funimage "0.1.62"
   :description "Functional Image Processing with ImageJ/FIJI"
   :url "https://github.com/funimage/funimage"
   :license {:name "Apache v2.0"
