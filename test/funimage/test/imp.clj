24ca04c308d8a340aaddd1913892a14e92ba48ee
Kyle Harrington <kyleh@cs.brandeis.edu>
2015-08-19 12:41:47 -0400

    difference test

--- test/funimage/test/imp.clj
+++ test/funimage/test/imp.clj
@@ -6,12 +6,4 @@
   (let [imp (create-imp :width 10 :height 10)]
     (is imp)))
 
-#_(let [filename "/Volumes/Amnes/Claudia/ERG_Dll4_VEcad_IsoB4/07-06-2015/Ctl_P5_IsoB4594_ERG405_Dll4647_VEcad488b_Stitch.czi"
-     imp (open-imp filename)
-     channels (split-channels imp)]
-   (println (get-stack-depth imp))
-   (println "Channels:" (count channels))
-   (doseq [ch channels]
-     (show-imp ch)
-     (println ch (get-stack-depth ch))))
 
--- test/funimage/test/imp/calculator.clj
+++ test/funimage/test/imp/calculator.clj
@@ -8,3 +8,8 @@
         imp2 (imp-subtract imp1 imp1)]
     (is (zero? (:mean (get-image-statistics imp2))))))
 
+(deftest test-create-like-difference
+  (let [imp1 (ij.IJ/openImage "http://imagej.nih.gov/ij/images/boats.gif")
+        imp2 (create-imp-like imp1)
+        imp3 (imp-difference imp1 imp2)]
+    (is (zero? (:mean (get-image-statistics imp3))))))
