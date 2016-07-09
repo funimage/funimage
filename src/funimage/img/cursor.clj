8037306eefb4ae28dad70ef7692faa340a308701
Kyle Harrington <kharrington@uidaho.edu>
2016-07-09 17:01:46 +0200

    Cleaning funimage to be more idiomatic

--- src/funimage/img/cursor.clj
+++ src/funimage/img/cursor.clj
@@ -11,70 +11,70 @@
            [net.imglib2.view Views IntervalView]
            [net.imglib2 Cursor RandomAccess RandomAccessibleInterval Interval]))
     
-(defn cursor-get-val
+(defn get-val
   "Get the value of a numeric (RealType) cursor."
   [^Cursor cur]
   (.get ^net.imglib2.type.numeric.RealType (.get cur)))
 
-(defn cursor-set-val
+(defn set-val
   "Get the value of a numeric (RealType) cursor."
   [^Cursor cur val]
   (.set ^net.imglib2.type.numeric.RealType (.get cur) val))
 
-(defn cursor-set-byte-val
+(defn set-byte-val
   "Get the value of a numeric (RealType) cursor."
   [^Cursor cur ^long val]
   (.setInteger ^net.imglib2.type.numeric.integer.GenericByteType (.get cur) val))
 
-(defn cursor-inc
+(defn inc
   "Increment the value at a cursor."
   [^Cursor cur]
   (.inc ^net.imglib2.type.numeric.RealType (.get cur)))
 
-(defn cursor-dec
+(defn dec
   "Decrement the value at a cursor."
   [^Cursor cur]
   (.dec ^net.imglib2.type.numeric.RealType (.get cur)))
 
-(defn cursor-set-one
+(defn set-one
   "Set a cursor's value to one."
   [^Cursor cur]
   (.setOne ^net.imglib2.type.numeric.RealType (.get cur)))
 
-(defn cursor-set-zero
+(defn set-zero
   "Set a cursor's value to zero."
   [^Cursor cur]
   (.setZero ^net.imglib2.type.numeric.RealType (.get cur)))
 
-(defn cursor-add
+(defn add
   "Add 2 cursors together."
   [^Cursor cur1 ^Cursor cur2]
   (.add 
     ^net.imglib2.type.numeric.RealType (.get cur1)
     ^net.imglib2.type.numeric.RealType (.get cur2)))
 
-(defn cursor-mul
+(defn mul
   "Multiply 2 cursors together."
   [^Cursor cur1 ^Cursor cur2]
   (.mul 
     ^net.imglib2.type.numeric.RealType (.get cur1)
     ^net.imglib2.type.numeric.RealType (.get cur2)))
 
-(defn cursor-sub
+(defn sub
   "Subtract 2 cursors together."
   [^Cursor cur1 ^Cursor cur2]
   (.sub
     ^net.imglib2.type.numeric.RealType (.get cur1)
     ^net.imglib2.type.numeric.RealType (.get cur2)))
 
-(defn cursor-div
+(defn div
   "Divide one cursor by another."  
   [^Cursor cur1 ^Cursor cur2]
   (.div
     ^net.imglib2.type.numeric.RealType (.get cur1)
     ^net.imglib2.type.numeric.RealType (.get cur2)))
 
-(defn cursor-sum-neighborhood
+(defn sum-neighborhood
   "Sum a neighborhood"
   [nbrhood]
   (loop [sum 0
--- test/funimage/test/img.clj
+++ test/funimage/test/img.clj
@@ -1,23 +1,24 @@
 (ns funimage.test.img
-  (:use [funimage imp img conversion]
-        [funimage.img cursor]
-        [clojure.test]))
+  (:use [funimage imp conversion]        
+        [clojure.test])
+  (:require [funimage.img :as img]
+            [funimage.img.cursor :as cursor]))
 
 (deftest test-img-add
   (let [w 5 h 5
-        img1 (first (map-imgs cursor-set-one
+        img1 (first (img/map cursor/set-one
                                (imp->img (create-imp :width w :height h))))
-        img2 (first (map-imgs cursor-set-one
+        img2 (first (img/map cursor/set-one
                                (imp->img (create-imp :width w :height h))))
-        img-res (first (map-imgs cursor-add
+        img-res (first (img/map cursor/add
                                   img1 img2))
-        img-sum (sum-img img-res)]
+        img-sum (img/sum img-res)]
     (is (= img-sum (* 2 w h)))))
 
 (deftest test-img-inc
   (let [w 5 h 5
-        img (first (map-imgs cursor-inc
-                              (imp->img (create-imp :width w :height h))))
-        img-sum (sum-img img)]
+        img (first (img/map cursor/inc
+                            (imp->img (create-imp :width w :height h))))
+        img-sum (img/sum img)]
     (is (= img-sum (* w h)))))
     
