9f1f83874acbd1a608f9adb356d2d1d700395a86
Kyle Harrington <kharrington@uidaho.edu>
2016-09-28 15:25:40 -0400

    Deploy master only

---
+++ funimage_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+   lein deploy
+   curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
+   unzip fiji-nojre.zip
+   mv target/funimage* Fiji.app/jars/
+   cp -r src/plugins/* Fiji.app/plugins/
+   cd Fiji.app
+   curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
+   chmod a+x upload-site-simple.sh
+   ./upload-site-simple.sh Funimage Funimage   
+fi
