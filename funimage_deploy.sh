64c8c556985ff7536e03b8014c41f100e09f699d
Kyle Harrington <kharrington@uidaho.edu>
2016-09-28 15:29:59 -0400

    fixing deploy script

--- funimage_deploy.sh
+++ funimage_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == 'master' ]]
+then
    lein deploy
    curl -O http://downloads.imagej.net/fiji/latest/fiji-nojre.zip
    unzip fiji-nojre.zip
@@ -9,5 +10,5 @@ if [[ $TRAVIS_BRANCH == 'master' ]]
    cd Fiji.app
    curl -O https://raw.githubusercontent.com/fiji/fiji/7f13f66968a9d4622e519c8aae04786db6601314/bin/upload-site-simple.sh
    chmod a+x upload-site-simple.sh
-   ./upload-site-simple.sh Funimage Funimage   
+   ./upload-site-simple.sh Funimage Funimage
 fi
