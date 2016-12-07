4be37e0e4fa3c8be98f79d845fb26f7d2662281c
Kyle Harrington <kharrington@uidaho.edu>
2016-12-07 14:19:27 +0100

    bump ops

--- src/funimage/imagej/ops.clj
+++ src/funimage/imagej/ops.clj
@@ -27,43 +27,44 @@
 
 (def valid-ops (into #{} (.ops (.op ij/ij))))
 
-
-(doall
-  (for [[op-name op-infos] (group-by #(.getName %)
-                                     (filter #(valid-ops (.getName %))
-                                             (filter #(not (infos-ignore (.getName %)))
-                                                     (.infos (.op ij/ij)))))
-        op-info op-infos]
-    (eval 
-      (let [expr (with-out-str
-                   (let [op-expression "(.op ij/ij)"]
-                     ;(.println (System/err) (str "defn " (.getName op-info) op-info))
-                     ;(.println (System/err) (str "defn " (.getName op-info) " " (.getClassName (.cInfo op-info))))
-                     (let [parts (string/split (.getName op-info) #"\.")
-                           cinfo (.cInfo op-info)
-                           required-inputs (filter #(.isRequired %) (seq (.inputs cinfo)))
-                           optional-inputs (filter #(not (.isRequired %)) (seq (.inputs cinfo)))
-                           arg-list (string/join
-                                      " "
-                                      (map #(str (make-typehint (.getType %)) " " (.getName %))
-                                           required-inputs))
-                           args-to-pass (string/join
-                                          " "
-                                          (map #(.getName %)
-                                               required-inputs))
-                           op-name (str op-name (when (> (count op-infos) 1)
-                                                  (let [classname (.getClassName (.cInfo op-info))
-                                                        tail-part (last (string/split classname #"\."))
-                                                        extension (if (.contains tail-part "$")
-                                                                    (last (string/split tail-part #"\$"))
-                                                                    tail-part)]
-                                                    (str "." extension))))]
-                       #_(.println (System/err) (string/join " " (map #(.getType %) (seq (.inputs cinfo)))))
-                       (println "(defn" (string/replace op-name "." "-"))
-                       (println "\t\"" (.getTitle cinfo) "\"")
-                       (println "\t[" arg-list "]")
-                       (println (str "\t(." (second parts) " (." (first parts) " " op-expression ") " args-to-pass "))")))))]
-        ;(println expr)
-        (read-string expr)))))
+(def op-list
+  (doall
+    (for [[op-name op-infos] (group-by #(.getName %)
+                                       (filter #(valid-ops (.getName %))
+                                               (filter #(not (infos-ignore (.getName %)))
+                                                       (.infos (.op ij/ij)))))
+          op-info op-infos]
+      (eval 
+        (let [expr (with-out-str
+                     (let [op-expression "(.op ij/ij)"]
+                       ;(.println (System/err) (str "defn " (.getName op-info) op-info))
+                       ;(.println (System/err) (str "defn " (.getName op-info) " " (.getClassName (.cInfo op-info))))
+                       (let [parts (string/split (.getName op-info) #"\.")
+                             cinfo (.cInfo op-info)
+                             required-inputs (filter #(.isRequired %) (seq (.inputs cinfo)))
+                             optional-inputs (filter #(not (.isRequired %)) (seq (.inputs cinfo)))
+                             arg-list (string/join
+                                        " "
+                                        (map #(str (make-typehint (.getType %)) " " (.getName %))
+                                             required-inputs))
+                             args-to-pass (string/join
+                                            " "
+                                            (map #(.getName %)
+                                                 required-inputs))
+                             op-name (str op-name (when (> (count op-infos) 1)
+                                                    (let [classname (.getClassName (.cInfo op-info))
+                                                          tail-part (last (string/split classname #"\."))
+                                                          extension (string/replace tail-part "$" "-")
+                                                                      #_(if (.contains tail-part "$")
+                                                                         (last (string/split tail-part #"\$"))
+                                                                         tail-part)]
+                                                      (str "." extension))))]
+                         #_(.println (System/err) (string/join " " (map #(.getType %) (seq (.inputs cinfo)))))
+                         (println "(defn" (string/replace op-name "." "-"))
+                         (println "\t\"" (.getTitle cinfo) "\"")
+                         (println "\t[" arg-list "]")
+                         (println (str "\t(." (second parts) " (." (first parts) " " op-expression ") " args-to-pass "))")))))]
+          ;(println expr)
+          (read-string expr))))))
 
 
