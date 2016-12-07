a6702837197a01851206737ef27995b87d7b6f30
Kyle Harrington <kharrington@uidaho.edu>
2016-12-07 14:07:48 +0100

    Adding a commented snippet for posterity

--- test/funimage/test/imagej/ops.clj
+++ test/funimage/test/imagej/ops.clj
@@ -8,4 +8,13 @@
 #_(let [fi (net.imglib2.FinalInterval. (long-array [10 10]))
        tpe (net.imglib2.type.numeric.real.DoubleType.)
        img1 (ops/create-img-CreateImgFromDimsAndType fi tpe)]
-   (show-imp (img->imp img1)))
+   (show-imp (img->imp (ops/math-add-ConstantToArrayImage-AddInt img1 17))))
+
+#_(let [cat (imp->img (open-imp "/Users/kharrington/git/funimage/black_cat.tif"))
+       hat (imp->img (open-imp "/Users/kharrington/git/funimage/witch_hat_small.tif"))
+       shape (net.imglib2.algorithm.neighborhood.RectangleShape. 3 false)
+       adder (fn [img1 img2] (imp-flatten (add-overlay-image (copy-imp (img->imp img1)) (img->imp img2) 110 50 100 true)))]
+   (show-imp (tile-imps
+               (map (fn [func] (adder cat (func (img/copy hat) shape)))
+                    [ops/morphology-dilate-DefaultDilate ops/morphology-erode-DefaultErode]))))
+
