ff91f4420604a3bb85d9cc9c57bbc6cee4656e2f
Kyle I S Harrington <kephale@users.noreply.github.com>
2016-12-30 10:47:51 -0800

    Ops matcher (#26)
    * Creating namespaces, populating them with functions, not yet all varargs
    
    * Dynamic loading of multiarg ops
    
    * New ops calling works, imp tests broken
    
    * Fixing visual demo from ops test
    
    * Cleaning ops ns

--- src/funimage/imagej/ops.clj
+++ src/funimage/imagej/ops.clj
@@ -5,17 +5,13 @@
 (defn make-typehint
   "Make the typehint for a command input's type."
   [input-type]
-  ;(.println (System/err) (str input-type " " (.toString input-type)))
-  ;(.println (System/err) (str input-type " " (.toString input-type)))
   (if-not input-type
     ""
     (let [type-string (.toString input-type)
           parts (string/split type-string #" ")]
       (if (= 1 (count parts))
-        ;(str "^" (first parts))
         ""
         (let [classname (second parts)]
-          ;(.println (System/err) (string/join " " parts))
           (cond (= classname "[J") "^longs"
                 (= classname "[I") "^ints"
                 (= classname "[D") "^doubles"
@@ -26,17 +22,14 @@
 (defn guess-type
   "Guess the type of a command input's type."
   [input-type]
-  ;(.println (System/err) (str input-type " " (.toString input-type)))
-  ;(.println (System/err) (str input-type " " (.toString input-type)))
+  
   (if-not input-type
     ""
     (let [type-string (.toString input-type)
           parts (string/split type-string #" ")]
       (if (= 1 (count parts))
-        ;(str "^" (first parts))
         ""
         (let [classname (second parts)]
-          ;(.println (System/err) (string/join " " parts))
           (cond (= classname "[J") "longs"
                 (= classname "[I") "ints"
                 (= classname "[D") "doubles"
@@ -48,87 +41,60 @@
 
 (def valid-ops (into #{} (.ops (.op ij/ij))))
 
-#_(def op-list
-   (doall
-     (for [[op-name op-infos] (group-by #(.getName %)
-                                        (filter #(valid-ops (.getName %))
-                                                (filter #(not (infos-ignore (.getName %)))
-                                                        (.infos (.op ij/ij)))))
-           op-info op-infos]
-       (eval 
-         (let [expr (with-out-str
-                      (let [op-expression "(.op ij/ij)"]
-                        (let [parts (string/split (.getName op-info) #"\.")
-                              cinfo (.cInfo op-info)
-                              required-inputs (filter #(.isRequired %) (seq (.inputs cinfo)))
-                              optional-inputs (filter #(not (.isRequired %)) (seq (.inputs cinfo)))
-                              arg-list (string/join
-                                         " "
-                                         (map #(str (make-typehint (.getType %)) " " (.getName %))
-                                              required-inputs))
-                              args-to-pass (string/join
-                                             " "
-                                             (map #(.getName %)
-                                                  required-inputs))
-                              op-name (str op-name (when (> (count op-infos) 1)
-                                                     (let [classname (.getClassName (.cInfo op-info))
-                                                           tail-part (last (string/split classname #"\."))
-                                                           extension (string/replace tail-part "$" "-")
-                                                                       #_(if (.contains tail-part "$")
-                                                                          (last (string/split tail-part #"\$"))
-                                                                          tail-part)]
-                                                       (str "." extension))))]
-                          (println "(defn" (string/replace op-name "." "-"))
-                          (println "\t\"" (.getTitle cinfo) "\"")
-                          (println "\t[" arg-list "]")
-                          (println (str "\t(." (second parts) " (." (first parts) " " op-expression ") " args-to-pass "))")))))]
-           ;(println expr)
-           (read-string expr))))))
+(def ops-namespaces (atom []))
 
 (def op-list
   (doall
     (for [[op-name op-infos] (group-by #(.getName %)
                                        (filter #(valid-ops (.getName %))
                                                (filter #(not (infos-ignore (.getName %)))
-                                                       (.infos (.op ij/ij)))))
-          op-info op-infos]
-      (let [op-expression "(.op ij/ij)"
+                                                       (.infos (.op ij/ij)))))]
+      (let [op-info (first op-infos)
+            op-expression "(.op ij/ij)"
             parts (string/split (.getName op-info) #"\.")
             cinfo (.cInfo op-info)
-            required-inputs (filter #(.isRequired %) (seq (.inputs cinfo)))
-            optional-inputs (filter #(not (.isRequired %)) (seq (.inputs cinfo)))
-            arg-list (string/join
-                       " "
-                       (map #(str (let [tpe (guess-type (.getType %)) ]
-                                    (if (empty? tpe)
-                                      ""
-                                      (str "^"tpe))) 
-                                  " " (.getName %))
-                            required-inputs))
-            args-to-pass (string/join
-                           " "
-                           (map #(.getName %)
-                                required-inputs))
-            op-name (str op-name (when (> (count op-infos) 1)
-                                   (let [classname (.getClassName (.cInfo op-info))
-                                         tail-part (last (string/split classname #"\."))
-                                         extension (string/replace tail-part "$" "-")
-                                                     #_(if (.contains tail-part "$")
-                                                        (last (string/split tail-part #"\$"))
-                                                        tail-part)]
-                                     (str "." extension))))
-            function-name (string/replace op-name "." "-")
+            op-namespace (symbol (string/join
+                                   "."
+                                   (concat [(ns-name *ns*)]
+                                         (butlast (string/split op-name #"\.")))))
+            function-name (last (string/split op-name #"\."))
             doc-string (.getTitle cinfo)
+
+            fn-defs (doall
+                      (for [op-info op-infos]
+                        (let [cinfo (.cInfo op-info)                            
+                              required-inputs (filter #(.isRequired %) (seq (.inputs cinfo)))
+                              optional-inputs (filter #(not (.isRequired %)) (seq (.inputs cinfo)))
+                              arg-list (string/join
+                                         " "
+                                         (map #(str (let [tpe (guess-type (.getType %)) ]
+                                                      (if (empty? tpe)
+                                                        ""
+                                                        (str "^"tpe))) 
+                                                    " " (.getName %))
+                                              required-inputs))
+                              args-to-pass (string/join
+                                             " "
+                                             (map #(.getName %)
+                                                  required-inputs))]
+                          [(count required-inputs)
+                           (with-out-str
+                             (println "([" arg-list "]")
+                             (println (str "\t(." (second parts) " (." (first parts) " " op-expression ") " args-to-pass "))")))])))
+            arity-map (apply hash-map (flatten fn-defs))
             expr (read-string
-                   (with-out-str
-                     (println "(defn" function-name)
-                     (println "\t\"" doc-string "\"")
-                     (println "\t[" arg-list "]")
-                     (println (str "\t(." (second parts) " (." (first parts) " " op-expression ") " args-to-pass "))"))))]
-        (eval expr)
+                   (str "(fn " (string/join " " (vals arity-map)) ")"))]        
+        ; Test if the NS exists, if it doesn't then make it          
+        (when-not (try (ns-name op-namespace) (catch Exception e nil))
+          (println "Making ns:" op-namespace)          
+          (create-ns op-namespace)
+          (swap! ops-namespaces conj op-namespace))
+        ; Make the function and load it into the respective namespace
+        (intern (the-ns op-namespace)
+                (symbol function-name)
+                (eval expr))
+        ; This doesnt need to be captured anymore
         {:function-name function-name
-         :input-types (map #(guess-type (.getType %))
-                           required-inputs)
-         :output-types (map #(guess-type (.getType %))
-                            (seq (.outputs cinfo)))
+         :expression expr
+         :namespace op-namespace
          }))))
--- test/funimage/test/imagej/ops.clj
+++ test/funimage/test/imagej/ops.clj
@@ -5,23 +5,12 @@
             [funimage.img.cursor :as cursor]
             [funimage.imagej :as ij]
             [funimage.imagej.ops :as ops]
+            ;[funimage.imagej.ops.create :as create]
+            ;[funimage.imagej.ops.morphology :as morphology]
             [funimage.img.utils :as img-utils]))
 
-#_(let [fi (net.imglib2.FinalInterval. (long-array [10 10]))
-       tpe (net.imglib2.type.numeric.real.DoubleType.)
-       img1 (ops/create-img-CreateImgFromDimsAndType fi tpe)]
-   (show-imp (img->imp (ops/math-add-ConstantToArrayImage-AddInt img1 17))))
-
-#_(let [cat (imp->img (open-imp "/Users/kharrington/git/funimage/black_cat.tif"))
-       hat (imp->img (open-imp "/Users/kharrington/git/funimage/witch_hat_small.tif"))
-       shape (net.imglib2.algorithm.neighborhood.RectangleShape. 3 false)
-       adder (fn [img1 img2] (imp-flatten (add-overlay-image (copy-imp (img->imp img1)) (img->imp img2) 110 50 100 true)))]
-   (show-imp (tile-imps
-               (map (fn [func] (adder cat (func (img/copy hat) shape)))
-                    [ops/morphology-dilate-DefaultDilate ops/morphology-erode-DefaultErode]))))
-
 (deftest test-ops
-  (let [img (ops/create-img-CreateImgFromDimsAndType (net.imglib2.FinalInterval. (long-array [10 10])) (net.imglib2.type.numeric.real.DoubleType.))]    
+  (let [img (funimage.imagej.ops.create/img (net.imglib2.FinalInterval. (long-array [10 10])) (net.imglib2.type.numeric.real.DoubleType.))]    
     (is img)))
 
 #_(do 
@@ -32,6 +21,6 @@
      (ij/show (img-utils/tile-imgs
                 (map (fn [func] (adder (img/copy cat) (func (img/copy hat))))
                      (mapcat (fn [r]
-                               [#(ops/morphology-dilate-DefaultDilate % (net.imglib2.algorithm.neighborhood.RectangleShape. r true))
-                                #(ops/morphology-erode-DefaultErode % (net.imglib2.algorithm.neighborhood.RectangleShape. r true))])
+                               [#(funimage.imagej.ops.morphology/dilate % (net.imglib2.algorithm.neighborhood.RectangleShape. r true))
+                                #(funimage.imagej.ops.morphology/erode % (net.imglib2.algorithm.neighborhood.RectangleShape. r true))])
                              (range 3 7)))))))
