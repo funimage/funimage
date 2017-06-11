63d340736352995743511b5af4b2aa036529ee71
Kyle Harrington <kharrington@uidaho.edu>
2017-06-11 16:16:04 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funimage "0.1.98-SNAPSHOT"
+(defproject funimage "0.1.98"
   :description "Functional Image Processing with ImageJ/FIJI"
   :url "https://github.com/funimage/funimage"
   :license {:name "Apache v2.0"
